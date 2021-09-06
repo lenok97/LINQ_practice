87d395158d25e3616d320f73ed1af204c809bc02
lenok97 <iamhappluck@gmail.com>
2021-09-06 20:07:25 +0500

    selectMany

---
+++ Classroom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ.Classroom
+{
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            CheckStudents();
+            Console.ReadKey();
+        }
+
+        public static void CheckStudents()
+        {
+            Classroom[] classes =
+            {
+                new Classroom {Students = {"Pavel", "Ivan", "Petr"},},
+                new Classroom {Students = {"Anna", "Ilya", "Vladimir"},},
+                new Classroom {Students = {"Bulat", "Alex", "Galina"},}
+            };
+
+            var allStudents = GetAllStudents(classes);
+            Array.Sort(allStudents);
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        public static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(l => l.Students).ToArray();
+        }
+    }
+}
