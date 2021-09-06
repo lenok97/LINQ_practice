385b199f5f0da73b220852ab12f4e678453105bc
lenok97 <iamhappluck@gmail.com>
2021-09-06 20:05:30 +0500

    task 1. Parse numbers from lines

---
+++ ParseNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ.ParseNumbers
+{
+    class Program
+    {
+        
+        public static void Main()
+        {
+            CheckParseNumbers();
+            Console.ReadKey();
+        }
+
+        public static int[] ParseNumbers(IEnumerable<string> lines)
+        {
+            return lines
+                .Where(l => l != "")
+                .Select(l => int.Parse(l)).ToArray();
+        }
+
+        public static void CheckParseNumbers()
+        {
+            foreach (var num in ParseNumbers(new[] { "-0", "+0000" }))
+                Console.WriteLine(num);
+            foreach (var num in ParseNumbers(new List<string> { "1", "", "-03", "0" }))
+                Console.WriteLine(num);
+        }
+    }
+}
